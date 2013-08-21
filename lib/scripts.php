a0ffc354c2ae76df64bdde56dbce7edad26590c3
Adam B <gadimus@gmail.com>
2013-08-21 17:46:15 -0600

    Added Script Enqueue(s)

--- lib/scripts.php
+++ lib/scripts.php
@@ -37,6 +37,8 @@ function roots_scripts() {
   wp_enqueue_script('jquery');
   wp_enqueue_script('modernizr');
   wp_enqueue_script('roots_plugins');
+  wp_enqueue_script('fitvids');
+  wp_enqueue_script('flexslider');
   wp_enqueue_script('roots_main');
 }
 add_action('wp_enqueue_scripts', 'roots_scripts', 100);
