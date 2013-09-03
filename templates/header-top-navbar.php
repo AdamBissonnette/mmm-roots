4222c4e6abaa9b944be9cef1416935f15b05a789
Adam B <Adam@Adams-iMac.local>
2013-09-03 14:48:41 -0600

    Jacpine Press Changes
    Added search bar to header, added a background image

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -1,5 +1,15 @@
 <header class="banner navbar navbar-default navbar-static-top" role="banner">
   <div class="container">
+    <form role="search" method="get" class="search-form form-inline" action="<?php echo home_url('/'); ?>">
+      <div class="input-group">
+        <input type="search" value="<?php if (is_search()) { echo get_search_query(); } ?>" name="s" class="search-field form-control" placeholder="<?php _e('Search', 'roots'); ?> <?php bloginfo('name'); ?>">
+        <label class="hide"><?php _e('Search for:', 'roots'); ?></label>
+        <span class="input-group-btn">
+          <button type="submit" class="search-submit btn btn-default"><i class="icon-search"></i></button>
+        </span>
+      </div>
+    </form>
+
     <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
         <span class="sr-only">Toggle navigation</span>
