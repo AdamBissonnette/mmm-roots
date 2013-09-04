dc52ade7ab0b065901694fee01966284f3674c06
Adam B <Adam@Adams-iMac.local>
2013-09-04 14:51:24 -0600

    Updated home to include content
    Content can now be added the standard area to the homepage and will
    appear just below the jumbotron

--- page-home.php
+++ page-home.php
@@ -9,3 +9,11 @@ global $MMM_Roots;
     if ($MMM_Roots->get_setting('jumbotron_category')) get_template_part('templates/content', 'jumbotron');
     get_template_part('templates/page-sections/section', 'listing');
 ?>
+
+<div class="wrap container" role="document">
+<div class="content row">
+  <div class="main col-lg-12" role="main">
+	<?php the_content(); ?>
+  </div><!-- /.main -->
+</div><!-- /.content -->
+</div><!-- /.wrap -->
