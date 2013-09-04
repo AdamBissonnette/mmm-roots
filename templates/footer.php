20ed7275d71efe61b1925b785fd555f982b5ed8f
Adam B <Adam@Adams-iMac.local>
2013-09-04 13:53:54 -0600

    Fixed footer styling, changed background

--- templates/footer.php
+++ templates/footer.php
@@ -7,17 +7,17 @@ $footerText = $MMM_Roots->get_setting("footer_text");
 <div class="footer-wrapper">
 	<footer class="content-info container" role="contentinfo">
 	  <div class="row sidebar">
-	    <div class="col-lg-12">
-	      <?php dynamic_sidebar('sidebar-footer'); ?>
-	    </div>
+      	<?php dynamic_sidebar('sidebar-footer'); ?>
 	  </div>
 	  <div class="row sitedetails">
+	  	<div class="col-lg-12">
 		<p class="pull-right">
 			<?php
 				echo stripslashes($footerText);
 			?>
 		</p>
 		<p>&copy; <?php bloginfo('name'); ?> <?php echo date('Y'); ?> - All rights reserved</p>
+	</div>
 	  </div>
 	</footer>
 </div>
