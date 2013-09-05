b21a19ecae31c3d6e129dc3032201caa8acd8023
Adam B <Adam@Adams-iMac.local>
2013-09-05 15:41:54 -0600

    Footer Updates
    Split the footer widgets and footer text into two separate wrappers /
    containers

--- templates/footer.php
+++ templates/footer.php
@@ -4,22 +4,28 @@ $footerText = $MMM_Roots->get_setting("footer_text");
 
 ?>
 
-<div class="footer-wrapper">
-	<footer class="content-info container" role="contentinfo">
+<div class="footer-widgets">
+	<div class="content-info container" role="contentinfo">
 	  <div class="row sidebar">
       	<?php dynamic_sidebar('sidebar-footer'); ?>
 	  </div>
-	  <div class="row sitedetails">
-	  	<div class="col-lg-12">
-		<p class="pull-right">
-			<?php
-				echo stripslashes($footerText);
-			?>
-		</p>
-		<p>&copy; <?php bloginfo('name'); ?> <?php echo date('Y'); ?> - All rights reserved</p>
-	</div>
-	  </div>
-	</footer>
+	 </div>
 </div>
+
+<footer>
+	<div class="container">
+		<div class="row sitedetails">
+			<div class="col-lg-12">
+				<p class="pull-right">
+					<?php
+						echo stripslashes($footerText);
+					?>
+				</p>
+				<p>&copy; <?php bloginfo('name'); ?> <?php echo date('Y'); ?> - All rights reserved</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
 
 <?php wp_footer(); ?>
