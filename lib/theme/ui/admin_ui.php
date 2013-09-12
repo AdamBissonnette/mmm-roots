b84747065f9c824e1fb2d0332d9639ad873f1c8d
Adam B <Adam@Adams-iMac.local>
2013-09-12 15:18:49 -0600

    Modal bug fixed
    Updated modal to match bootstrap 3 css / js

--- lib/theme/ui/admin_ui.php
+++ lib/theme/ui/admin_ui.php
@@ -27,17 +27,21 @@
 		</div>
 
 
-		<div class="modal hide fade" id="mm-dialog">
-		    <div class="modal-header">
-		        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-		        <h3 id="mm-dialog-title"></h3>
-		    </div>
-		    <div class="modal-body" id="mm-dialog-message">
-		    
-		    </div>
-		    <div class="modal-footer">
-		        <a href="#" data-dismiss="modal" class="btn">Close</a>
-		    </div>
+		<div class="modal fade" id="mm-dialog">
+			<div class="modal-dialog">
+				<div class="modal-content">
+				    <div class="modal-header">
+				        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+				        <h3 id="mm-dialog-title"></h3>
+				    </div>
+				    <div class="modal-body" id="mm-dialog-message">
+				    
+				    </div>
+				    <div class="modal-footer">
+				        <a href="#" data-dismiss="modal" class="btn">Close</a>
+				    </div>
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
