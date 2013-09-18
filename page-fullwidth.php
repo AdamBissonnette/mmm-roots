a0ae06ed36899540548d02b502664b2f0a43f8d0
Adam B <Adam@Adams-iMac.local>
2013-09-18 14:14:10 -0600

    Added New Page Types
    Also updated existing types to include page-sections

--- page-fullwidth.php
+++ page-fullwidth.php
@@ -1,14 +1,14 @@
+<?php
+/*
+Template Name: Fullwidth Page
+*/
+?>
 <div class="wrap container" role="document">
 <div class="content row">
-  <div class="main <?php echo roots_main_class(); ?>" role="main">
+  <div class="main col-lg-12" role="main">
     <?php get_template_part('templates/page', 'header'); ?>
 	<?php get_template_part('templates/content', 'page'); ?>
   </div><!-- /.main -->
-  <?php if (roots_display_sidebar()) : ?>
-  <aside class="sidebar <?php echo roots_sidebar_class(); ?>" role="complementary">
-    <?php include roots_sidebar_path(); ?>
-  </aside><!-- /.sidebar -->
-  <?php endif; ?>
 </div><!-- /.content -->
 </div><!-- /.wrap -->
 
--- page-leftsidebar.php
+++ page-leftsidebar.php
@@ -1,14 +1,19 @@
+<?php
+/*
+Template Name: Left Sidebar Page
+*/
+?>
 <div class="wrap container" role="document">
 <div class="content row">
-  <div class="main <?php echo roots_main_class(); ?>" role="main">
-    <?php get_template_part('templates/page', 'header'); ?>
-	<?php get_template_part('templates/content', 'page'); ?>
-  </div><!-- /.main -->
   <?php if (roots_display_sidebar()) : ?>
   <aside class="sidebar <?php echo roots_sidebar_class(); ?>" role="complementary">
     <?php include roots_sidebar_path(); ?>
   </aside><!-- /.sidebar -->
   <?php endif; ?>
+  <div class="main <?php echo roots_main_class(); ?>" role="main">
+    <?php get_template_part('templates/page', 'header'); ?>
+	<?php get_template_part('templates/content', 'page'); ?>
+  </div><!-- /.main -->
 </div><!-- /.content -->
 </div><!-- /.wrap -->
 
