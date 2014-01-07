10f68d5d757956dfceee69872c788dff15678ade
Adam B <gadimus@gmail.com>
2014-01-07 12:43:21 -0600

    Updated Dialog html

--- base.php
+++ base.php
@@ -43,18 +43,22 @@
   });
   </script>
 
-  <div class="modal hide fade" id="mm-dialog">
-    <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-        <h3 id="mm-dialog-title">Message Sent!</h3>
-    </div>
-     <div class="modal-body" id="mm-dialog-message">
-        <p>Your message has been sent.  We will be in touch shortly!</p>
-     </div>
-     <div class="modal-footer">
-        <a href="#" data-dismiss="modal" class="btn">Close</a>
-      </div>
-  </div>
+  <div class="modal fade" id="mm-dialog" tabindex="-1" role="dialog" aria-labelledby="mm-dialog-title" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+          <h4 class="modal-title" id="mm-dialog-title"></h4>
+        </div>
+        <div class="modal-body" id="mm-dialog-message">
+          <p>Your message has been sent.  We will be in touch shortly!</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+  </div><!-- /.modal -->
 
 </body>
 </html>
